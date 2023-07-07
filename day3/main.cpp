fdf16da4649d78343cecc8f55ae7c39ab62f1253
sewell <sewelljohnathan@proton.me>
2023-07-07 11:19:09 -0400

    Clean day3 code

--- day3/main.cpp
+++ day3/main.cpp
@@ -9,35 +9,25 @@ int main(void)
 
     int count = 0;
 
-    std::vector<int> colA, colB, colC;
+    std::vector<int> nums;
     std::string s;
     while (inFile >> s) 
     {
-
-        colA.push_back(std::stoi(s));
+        nums.push_back(std::stoi(s));
         inFile >> s;
-        colB.push_back(std::stoi(s));
+        nums.push_back(std::stoi(s));
         inFile >> s;
-        colC.push_back(std::stoi(s));
-
+        nums.push_back(std::stoi(s));
     }
 
-    for (int i = 0; i < colA.size(); i += 3) 
-    {
-        int a = colA.at(i), b = colA.at(i+1), c = colA.at(i + 2);
-        count += (a + b > c && a + c > b && b + c > a);
-    }
-    for (int i = 0; i < colB.size(); i += 3) 
-    {
+    for (int i = 0; i < nums.size() - 6; i += 9) {
+
+        for (int j = i; j < i + 3; j++) 
+        {
+            int a = nums.at(j), b = nums.at(j + 3), c = nums.at(j + 6);
+            count += (a + b > c && a + c > b && b + c > a);
+        }
         
-        int a = colB.at(i), b = colB.at(i+1), c = colB.at(i + 2);
-        count += (a + b > c && a + c > b && b + c > a);
-    }
-    for (int i = 0; i < colC.size(); i += 3) 
-    {
-        
-        int a = colC.at(i), b = colC.at(i+1), c = colC.at(i + 2);
-        count += (a + b > c && a + c > b && b + c > a);
     }
 
     std::cout << count << std::endl;
